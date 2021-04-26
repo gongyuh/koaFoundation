5df70e97779158fae19a0eb78235edf544572b22
gongyuh <2736498007@qq.com>
2021-04-26 22:46:47 +0800

    update

---
+++ koa-found/src/index.js
@@ -0,0 +1,28 @@
+import koa from 'koa'
+import path from 'path' 
+
+import helmet from 'koa-helmet' 
+import statics from 'koa-static' 
+import router from './routes/routes' 
+import koaBody from 'koa-body' 
+import jsonutil from 'koa-json' 
+import cors from '@koa/cors'
+import compose from 'koa-compose'
+
+const app = new koa()
+
+// app.use(helmet())
+// app.use(statics(path.join(__dirname,'../public')))
+// app.use(router())
+const middleware = compose([
+    koaBody(),
+    statics(path.join(__dirname,'../public')),
+    cors(),
+    jsonutil({pretty:false,param:'pretty'}),
+    helmet()
+])
+
+app.use(middleware)
+app.use(router())
+
+app.listen(3000)
